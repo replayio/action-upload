d3d8cd943740f4f6e58376c22c686f27e3ee1a6d
Ryan Duffy <ryan@replay.io>
2022-03-28 13:04:35 -0700

    Add upload action

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# action-upload
-GitHub Action to upload recordings to Replay.io
+# `replayio/action-upload`
+
+> Upload recordings to [Replay](https://replay.io)
+
+## Usage
+
+1. Log into [app.replay.io](https://app.replay.io)
+2. Create a [Team API key](https://docs.replay.io/docs/setting-up-a-team-f5bd9ee853814d6f84e23fb535066199#4913df9eb7384a94a23ccbf335189370) (Personal API keys can be used, but have a limit of 10 recordings)
+3. Store the API key as a [GitHub Repository Secret](https://docs.github.com/en/actions/security-guides/encrypted-secrets#creating-encrypted-secrets-for-a-repository) named `RECORD_REPLAY_API_KEY`
+4. Add the configuration below to your existing workflow (or start a new one with the [complete example](#complete-workflow-example) below)
+
+```yaml
+- uses: replayio/action-upload
+  with:
+    apiKey: ${{ secrets.RECORD_REPLAY_API_KEY }}
+```
+
+## Arguments
+
+Required | Name | Description | Default
+-------- | ---- | ----------- | -------
+:white_check_mark: | `apiKey` | The Replay API Key used to upload recordings
+&nbsp; | `filter` | A [JSONata](https://jsonata.org/) function to pass to `[$filter](https://docs.jsonata.org/higher-order-functions#filter)` to select replays to upload | 
+&nbsp; | `public` | When true, make replays public on upload | `false`
+
+## Returns
+
+Name | Description
+---- | -----------
+`recordings` | An array of recording IDs uploaded
---
+++ upload.js
@@ -0,0 +1,106 @@
+async function upload(cli, jsonata, filter) {
+  const allRecordings = cli.listAllRecordings();
+
+  let recordings = allRecordings;
+  if (filter) {
+    const exp = jsonata(`$filter($, ${filter})`);
+    recordings = exp.evaluate(allRecordings) || [];
+  }
+
+  console.log(
+    "Processing",
+    recordings.length,
+    "of",
+    allRecordings.length,
+    "total recordings"
+  );
+
+  let failed = [];
+  let success = [];
+  for await (let r of recordings) {
+    try {
+      success.push(await cli.uploadRecording(r.id, { verbose: true }));
+    } catch (e) {
+      failed.push(e);
+    }
+  }
+
+  failed.forEach((reason) => {
+    console.error("Failed to upload replay:", reason);
+  });
+
+  return success;
+}
+
+async function makeReplaysPublic(axios, apiKey, recordings) {
+  const results = await Promise.allSettled(
+    recordings.map((r) => {
+      const variables = {
+        recordingId: r.recordingId,
+        isPrivate: false,
+      };
+
+      return axios({
+        url: "https://api.replay.io/v1/graphql",
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${apiKey}`,
+        },
+        data: {
+          query: `
+            mutation MakeReplayPublic($recordingId: ID!, $isPrivate: Boolean!) {
+              updateRecordingPrivacy(input: { id: $recordingId, private: $isPrivate }) {
+                success
+              }
+            }
+          `,
+          variables,
+        },
+      }).catch(e => {
+        if (e.response) {
+          console.log("Parameters");
+          console.log(JSON.stringify(variables, undefined, 2))
+          console.log("Response");
+          console.log(JSON.stringify(e.response.data, undefined, 2));
+        }
+
+        throw e.message;
+      });
+    })
+  );
+
+  results.forEach((r) => {
+    if (r.status === "rejected") {
+      console.error("Failed to mark replay public", r.reason);
+    }
+  });
+
+  return results.filter(r => r.status === "fulfilled");
+}
+
+async function uploadRecordings({ require, apiKey, filter, public = false }) {
+  try {
+    const cli = require("@replayio/replay");
+    const axios = require("axios");
+    const jsonata = require("jsonata");
+
+    const recordingIds = await upload(cli, jsonata, filter);
+    const uploaded = cli.listAllRecordings().filter(u => recordingIds.includes(u.recordingId));
+
+    console.log("Uploaded", recordingIds.length, "replays");
+
+    if (public && recordingIds.length > 0) {
+      const updated = await makeReplaysPublic(axios, apiKey, uploaded);
+      console.log("Marked", updated.length, "replays public");
+    }
+
+    return uploaded;
+  } catch (e) {
+    console.error("Failed to upload recordings");
+    console.error(e);
+
+    return [];
+  }
+}
+
+module.exports = uploadRecordings;
