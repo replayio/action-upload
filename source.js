719160dd7efa8d553bb33cda7b1e2b11c30dc714
Ryan Duffy <ryan@replay.io>
2022-05-17 13:58:47 -0700

    extract pr event data

--- source.js
+++ source.js
@@ -4,20 +4,27 @@ function buildSourceMetadata(source, eventPath) {
   if (fs.existsSync(eventPath)) {
     try {
       const event = JSON.parse(fs.readFileSync(eventPath));
-      console.log(JSON.stringify(event, undefined, 2));
-      return source.init({
-        branch: event.head?.ref,
-        commit: {
-          id: event.head?.sha,
-          title: event.commits?.[0].message
-        },
-        merge: event.pull_request?.number && {
-          id: event.pull_request.number,
-          title: event.pull_request.title,
-        },
-        provider: 'github',
-        repository: event.repo?.full_name
-      });
+      let metadata;
+
+      if (event.pull_request) {
+        const { pull_request: {head, number, title}, repository: {full_name} } = event;
+        metadata = {
+          branch: head.ref,
+          provider: 'github',
+          repository: full_name,
+          commit: {
+            id: head.sha
+          },
+          merge: {
+            id: number,
+            title
+          }
+        }
+      }
+
+      if (metadata) {
+        return source.init(metadata);
+      }
     } catch (e) {
       console.error("Failed to generate source control metadata");
       console.error(e);
