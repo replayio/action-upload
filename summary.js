127a3aa9c775db97bc8f3f3f1f39c7f372ac5345
Ryan Duffy <ryan@replay.io>
2023-06-02 11:53:29 -0700

    simplify table

--- summary.js
+++ summary.js
@@ -2,29 +2,23 @@ const core = require("@actions/core");
 const { SUMMARY_ENV_VAR } = require("@actions/core/lib/summary");
 
 async function addSummary({ cli, filter, includeSummary }) {
-  if (!includeSummary || !process.env[SUMMARY_ENV_VAR]) {
+  if (!includeSummary || !(SUMMARY_ENV_VAR in process.env)) {
     return;
   }
 
   const headers = [
     { data: "Title", header: true },
-    { data: "Link", header: true },
-    { data: "ID", header: true },
     { data: "Status", header: true },
   ];
 
   const recordings = cli.listAllRecordings({ all: true, filter });
 
-  const recordingRows = await Promise.all(
-    recordings.map(async (r) => [
-      r.metadata.title,
-      r.status === "uploaded"
-        ? `<a href="https://app.replay.io/recording/${r.id}" target="_blank">View</a>`
-        : "",
-      r.id,
-      r.status,
-    ])
-  );
+  const recordingRows = recordings.map((r) => [
+    r.status === "uploaded"
+      ? `<a href="https://app.replay.io/recording/${r.id}" target="_blank">${r.metadata.title}</a>`
+      : r.metadata.title,
+    r.status,
+  ]);
 
   await core.summary
     .addHeading("Replay Uploads", 2)
