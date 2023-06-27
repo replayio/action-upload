5e740cd0471d190ac2a8f35c98b39bb0a7b35f39
Ryan Duffy <ryan@replay.io>
2023-06-26 17:03:17 -0700

    Add test result to summary table when relevant

--- summary.js
+++ summary.js
@@ -1,6 +1,11 @@
 const core = require("@actions/core");
 const { SUMMARY_ENV_VAR } = require("@actions/core/lib/summary");
 
+const icons = {
+  passed: ":white_check_mark:",
+  failed: ":red_circle:",
+};
+
 async function addSummary({ cli, filter, includeSummary }) {
   if (!includeSummary || !(SUMMARY_ENV_VAR in process.env)) {
     return;
@@ -13,12 +18,35 @@ async function addSummary({ cli, filter, includeSummary }) {
 
   const recordings = cli.listAllRecordings({ all: true, filter });
 
-  const recordingRows = recordings.map((r) => [
-    r.status === "uploaded"
-      ? `<a href="https://app.replay.io/recording/${r.id}" target="_blank">${r.metadata.title}</a>`
-      : r.metadata.title,
-    r.status,
-  ]);
+  const hasTestMetadata = recordings.some((r) => r.metadata.test);
+  if (hasTestMetadata) {
+    headers.splice(1, 0, { data: "Test Result", header: true });
+  }
+
+  const recordingRows = recordings.map((r) => {
+    const title =
+      r.status === "uploaded"
+        ? `<a href="https://app.replay.io/recording/${r.id}" target="_blank">${r.metadata.title}</a>`
+        : r.metadata.title;
+    const status = r.status;
+
+    if (hasTestMetadata) {
+      const result = r.metadata.test?.result;
+      const resultCounts = r.metadata.test?.resultCounts;
+
+      if (resultCounts && resultCounts.passed > 0 && resultCounts.failed > 0) {
+        return [
+          title,
+          `${resultCounts.passed} ${icons.passed} / ${resultCounts.failed} ${icons.failed} / `,
+          status,
+        ];
+      }
+
+      return [title, icons[result] || "", status];
+    }
+
+    return [title, status];
+  });
 
   await core.summary
     .addHeading("Replay Uploads", 2)
